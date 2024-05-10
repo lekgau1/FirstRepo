380634e42d4cd737224da0e9623fc6d33b7e5330
lekgau1 <blekgau128@gmail.com>
2024-05-10 14:28:09 +0200

    First change

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner (System.In)
+        Scanner input = new Scanner(System.in);
         System.out.print("Your name: ");
         String name = input.next();
         System.out.println("welcome "+ name);
+        System.out.println("Hello world!");
 
 
     }
