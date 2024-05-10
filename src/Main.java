a460ba187218e32a5a51e050a6accb77635bbb5e
lekgau1 <blekgau128@gmail.com>
2024-05-10 13:54:22 +0200

    First change

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner (System.In)
+        System.out.print("Your name: ");
+        String name = input.next();
+        System.out.println("welcome "+ name);
+
+
     }
 }
